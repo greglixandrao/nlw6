251f74d33fff97cef90b0f4c6cac33c0cdcb9390
Greg Lixandrão <contato@greglixandrao.com>
2021-06-22 15:25:06 -0300

    Add database sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
+import "reflect-metadata";
 import express from "express"
 
+import "./database"
+
 const app = express()
-
-app.get("/test", (request, response) => {
-  return response.send("Olá Mundo NLW")
-})
-
-app.post("/test-post", (request, responde) => {
-  return responde.send("Olá POST NLW")
-})
 
 app.listen(3000, ()=> console.log("Server is Running"))
